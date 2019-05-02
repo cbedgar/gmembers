da07dbe69aa3d19385ae5c81baff3b67b7efa340
C. B. Edgar <cb@cbedgar.com>
2019-05-02 12:06:54 -0400

    Replaced tabs with spaces

--- gmembers.pl
+++ gmembers.pl
@@ -3,7 +3,7 @@
 # List all members of one or more unix groups, all groups or optionally just
 # the group specified on the command line
 #
-#	MIT License, see LICENSE file included with project
+#   MIT License, see LICENSE file included with project
 #
 # Copyright © 2013 William H. McCloskey, Jr., © 2019 Clement B. Edgar III
 # Copyright © 2010-2013 by Zed Pobre (zed@debian.org or zed@resonant.org)
@@ -38,26 +38,26 @@ foreach my $userid (@users)
     {
         $groupmembers{$group}->{$userid} = 1;
         $group_field_size < length $group
-        	and $group_field_size = length $group;
+            and $group_field_size = length $group;
     }
 }
 
 my @wantedgroups = @ARGV ? @ARGV : sort keys %groupmembers;
-	# look at specified groups if they exist, else look at all groups
+    # look at specified groups if they exist, else look at all groups
 
-my @groups_with_one;	# place groups with only a single user in them at the end
-my @groups_with_many;	# put the groups with multiple users at the front
+my @groups_with_one;    # place groups with only a single user in them at the end
+my @groups_with_many;   # put the groups with multiple users at the front
 foreach my $group (@wantedgroups) {
-	if ( keys %{$groupmembers{$group}} == 1) {
-		push @groups_with_one, $group;
-	}
-	else {
-		push @groups_with_many, $group;
-	}
+    if ( keys %{$groupmembers{$group}} == 1) {
+        push @groups_with_one, $group;
+    }
+    else {
+        push @groups_with_many, $group;
+    }
 }
 
 foreach my $group ( @groups_with_many, @groups_with_one ) {
-#    	print "DEBUG: \$group_field_size = $group_field_size\n";
-	printf( '%*s  :  ', -$group_field_size, $group );
-	print( join( ' ', sort keys %{$groupmembers{$group}}), "\n");
+#       print "DEBUG: \$group_field_size = $group_field_size\n";
+    printf( '%*s  :  ', -$group_field_size, $group );
+    print( join( ' ', sort keys %{$groupmembers{$group}}), "\n");
 }
